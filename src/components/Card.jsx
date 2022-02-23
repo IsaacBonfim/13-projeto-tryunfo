4ea79872aabb12c445f3d7a73316345b47907cbe
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-02-23 20:47:21 -0300

    Enviando o requisito 3

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,50 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class Card extends React.Component {
+class Card extends React.Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo } = this.props;
 
-// }
+    return (
+      <div>
+        <h1 data-testid="name-card">{ cardName }</h1>
 
-// Card.propTypes = {
-//   cardName: PropTypes.string.isRequired,
-//   cardDescription: PropTypes.string.isRequired,
-//   cardAttr1: PropTypes.string.isRequired,
-//   cardAttr2: PropTypes.string.isRequired,
-//   cardAttr3: PropTypes.string.isRequired,
-//   cardImage: PropTypes.string.isRequired,
-//   cardRare: PropTypes.string.isRequired,
-//   cardTrunfo: PropTypes.bool.isRequired,
-// };
+        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
 
-// export default Card;
+        <p data-testid="description-card">{ cardDescription }</p>
+
+        <span data-testid="attr1-card">{ cardAttr1 }</span>
+        <span data-testid="attr2-card">{ cardAttr2 }</span>
+        <span data-testid="attr3-card">{ cardAttr3 }</span>
+
+        <span data-testid="rare-card">{ cardRare }</span>
+
+        {
+          cardTrunfo ? <p data-testid="trunfo-card">Super Trunfo</p> : ''
+        }
+
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
+
+export default Card;
