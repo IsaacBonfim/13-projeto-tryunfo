b0f7179f7503cb1ee30345a0eaecfaa679e10d18
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2023-05-10 14:49:38 -0300

    Ajuste na forma como a raridade da carta é passada

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -102,7 +102,7 @@ class Form extends React.Component {
             >
               <option value="normal">Normal</option>
               <option value="raro">Raro</option>
-              <option value="muito raro">Muito raro</option>
+              <option value="m-raro">Muito raro</option>
             </select>
           </label>
 
